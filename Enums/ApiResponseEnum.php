3134dd4852b5b6818551cb6839739703444f546c
Kiều Trọng Thiện <kieutrongthien@gmail.com>
2022-11-12 23:38:42 +0700

    Update ApiResponseEnum.php

--- Enums/ApiResponseEnum.php
+++ Enums/ApiResponseEnum.php
@@ -8,10 +8,12 @@ use Omaicode\Enum\Enum;
  * @method static static SUCCESS( )
  * @method static static VALIDATION( )
  * @method static static SERVER_ERROR()
+ * @method static static UNAUTHORIZED()
  */
 final class ApiResponseEnum extends Enum
 {
     const SUCCESS      =  0;
     const VALIDATION   =  1;
     const SERVER_ERROR = -1;
+    const UNAUTHORIZED =  2;
 }
